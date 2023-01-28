00f4b6ddcabb70fcc45ac032d37a1a3441c2e9d0
Furkan Emir <74200375+Shakloniir@users.noreply.github.com>
2023-01-28 17:11:31 +0300

    Test

--- Assets/Game/Scripts/BlendshapeCollider.cs
+++ Assets/Game/Scripts/BlendshapeCollider.cs
@@ -3,12 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SphereCollider))]
+[RequireComponent(typeof(RASCALSkinnedMeshCollider))]
 public class BlendshapeCollider : MonoBehaviour
 {
     public SkinnedMeshRenderer rend;
     public RASCALSkinnedMeshCollider meshCollider;
     public float blendWeight;
 
+    private void Awake()
+    {
+        rend = GetComponentInChildren<SkinnedMeshRenderer>();
+        meshCollider = GetComponent<RASCALSkinnedMeshCollider>();
+        GetComponent<SphereCollider>().isTrigger = true;
+        meshCollider.immediateStartupCollision = true;
+        transform.tag = "Player";
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
