10fda0c023f2ab6a830af44530412cc8d4b04551
Furkan Emir <74200375+Shakloniir@users.noreply.github.com>
2023-01-30 08:51:23 +0300

    Last

--- Assets/Game/Scripts/BlendshapeCollider.cs
+++ Assets/Game/Scripts/BlendshapeCollider.cs
@@ -3,19 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(SphereCollider))]
+[RequireComponent(typeof(CapsuleCollider))]
 [RequireComponent(typeof(RASCALSkinnedMeshCollider))]
 public class BlendshapeCollider : MonoBehaviour
 {
     public SkinnedMeshRenderer rend;
     public RASCALSkinnedMeshCollider meshCollider;
+    public Animator animator;
     public float blendWeight;
+    public int counter;
 
     private void Awake()
     {
         rend = GetComponentInChildren<SkinnedMeshRenderer>();
         meshCollider = GetComponent<RASCALSkinnedMeshCollider>();
-        GetComponent<SphereCollider>().isTrigger = true;
+        animator = GetComponent<Animator>();
+        GetComponent<CapsuleCollider>().isTrigger = true;
         meshCollider.immediateStartupCollision = true;
         transform.tag = "Player";
     }
@@ -28,7 +31,29 @@ public class BlendshapeCollider : MonoBehaviour
     }
     public void StickmanFall()
     {
-        blendWeight += 10;
+        counter++;
+        animator.SetTrigger("Glup");
+        if (blendWeight <= 25)
+        {
+            blendWeight += 2f;
+        }
+        else if (blendWeight <= 45)
+        {
+            blendWeight += 2.5f;
+        }
+        else if (blendWeight <= 60)
+        {
+            blendWeight += 3f;
+        }
+        else if (blendWeight <= 100)
+        {
+            blendWeight += 3.5f;
+        }
+        else
+        {
+            //blow
+            animator.SetBool("Blow", true);
+        }
         rend.SetBlendShapeWeight(0, blendWeight);
         meshCollider.ImmediateUpdateColliders();
     }
--- Assets/Game/Scripts/Stickmans/StickmanFall.cs
+++ Assets/Game/Scripts/Stickmans/StickmanFall.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class StickmanFall : MonoBehaviour
 {
+    public bool control;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             GetComponent<RagdollEnable>().RagdollModeOn();
+            if (!control)
+                other.GetComponent<BlendshapeCollider>().StickmanFall();
+            control = true;
         }
     }
 }
